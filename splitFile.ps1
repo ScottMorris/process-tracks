b15da4d4d04088cc3451d81d1422b4051afcfb54
Scott Morris <scott.moris@gmail.com>
2020-10-29 22:32:33 -0400

    Update SplitFile.ps1
    - Reorganize variables so they aren't constanly created in the loop.
    - Add Genre support.

--- splitFile.ps1
+++ splitFile.ps1
@@ -4,7 +4,9 @@ param (
 	[Parameter(Mandatory=$true)][string]$inputFile,
 	[Parameter()][string]$album_artist,
 	[Parameter()][string]$album,
-	[Parameter()][string]$album_art
+	[Parameter()][string]$album_art,
+	[Parameter()][string]$genre
+
 	
 )
 
@@ -21,7 +23,6 @@ function CheckIfDirectoryExistsAndCreateIfNot {
 			Write-Error -Message "Unable to create directory '$DirectoryToCreate'. Error was: $_" -ErrorAction Stop
 		}
 		"Successfully created directory '$DirectoryToCreate'."
-	
 	}
 	else {
 		"Directory already existed"
@@ -29,31 +30,39 @@ function CheckIfDirectoryExistsAndCreateIfNot {
 }
 
 $trackNumberCount = 1
+
+$inputFileInfo = Get-Item $inputFile
+$inputFileDir = $inputFileInfo.DirectoryName
+$inputFileExtention = $inputFileInfo.Extension.Replace(".", "")
+$inFile = $inputFileInfo.FullName
+
+$outputAlbumName = ![string]::IsNullOrWhiteSpace($album) ? $album : "splitOutput"
+$outputBasePath = "$inputFileDir/$outputAlbumName"
+
+CheckIfDirectoryExistsAndCreateIfNot($outputBasePath)
+
 foreach($line in Get-Content $timecodeFileName) {
-	$inputFileInfo = Get-Item $inputFile
-	$inputFileDir = $inputFileInfo.DirectoryName
 	$startTime = $line.Substring(0, 11)
 	$endTime = $line.Substring(12, 11)
+
 	$name = $line.Substring(23).Trim()
 	$split = $name.Split("–")
 	$title = ($split.Length -gt 1) ? $split[1].Trim() : $null ?? $split[0].Trim()
 	$artist = ($split.Length -gt 1)  ? $split[0].Trim() : ""
-	$outputAlbumName = ![string]::IsNullOrWhiteSpace($album) ? $album : "splitOutput"
-	$outputBasePath = "$inputFileDir/$outputAlbumName"
-	$inputFileExtention = $inputFileInfo.Extension.Replace(".", "")
+
 	$currentTrack = $trackNumberCount++
 	$outFile = "$outputBasePath/{0:d2} $name.$inputFileExtention" -f $currentTrack
-	$inFile = $inputFileInfo.FullName
 
+	Write-Output "------"
 	Write-Output "Title: $title Artist: $artist"
 	Write-Output "Album Artist: $album_artist Album: $album"
+	Write-Output "------"
    
-	CheckIfDirectoryExistsAndCreateIfNot($outputBasePath)
-
 	if (![string]::IsNullOrWhiteSpace($album_art)) {
-		Write-Output "Album Art Not Implemented Yet"
+		Write-Output "Album Art Not Implemented Yet, Please Remove this Parameter."
+		exit -1
 		##&ffmpeg -hide_banner -i "$inFile" -i $album_art -map 0:0 -map 1:0 -metadata:s:v title="Album cover" -metadata:s:v comment="Cover (front)" -metadata title="$title" -metadata artist="$artist" -metadata album_artist="$album_artist" -metadata album="$album" -c copy -ss "$startTime" -to "$endTime" "$outFile"
 	} else {
-		&ffmpeg -hide_banner -i "$inFile" -metadata track="$currentTrack" -metadata title="$title" -metadata artist="$artist" -metadata album_artist="$album_artist" -metadata album="$album" -c:a copy -ss "$startTime" -to "$endTime" "$outFile"
+		&ffmpeg -hide_banner -i "$inFile" -metadata track="$currentTrack" -metadata title="$title" -metadata artist="$artist" -metadata album_artist="$album_artist" -metadata album="$album" -metadata genre="$genre" -c:a copy -ss "$startTime" -to "$endTime" "$outFile"
 	}
 }
